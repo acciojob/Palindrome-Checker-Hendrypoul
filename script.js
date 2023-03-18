1e9bb1325014f81b1c3fd203e4d0765f1875cb27
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-03-19 01:50:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,9 +4,9 @@ function palindrome(str){
 const len = str.length;
 	for(let i = 0; i < len/2; i++){
 		if(str[i] !== str[len - 1 - i]) {
-			return false;
+			return true;
 		}
 	}
-	return true;
+	return false;
 }
 module.exports = palindrome
